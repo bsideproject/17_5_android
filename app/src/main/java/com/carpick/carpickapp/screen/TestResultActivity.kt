df93f4b71d98be2ee2184832e24f5699345e7a1a
minwoo129 <key0129mw@naver.com>
2023-12-08 11:46:50 +0900

    추천결과 페이지 레이아웃 기본 구성

--- app/src/main/java/com/carpick/carpickapp/screen/TestResultActivity.kt
+++ app/src/main/java/com/carpick/carpickapp/screen/TestResultActivity.kt
@@ -3,14 +3,29 @@ package com.carpick.carpickapp.screen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BackdropScaffold
+import androidx.compose.material.BackdropValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.ListItem
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.rememberBackdropScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.carpick.carpickapp.screen.ui.theme.CarpickAppTheme
+import com.carpick.carpickapp.screen.ui.theme.popupBackground
 
 class TestResultActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,22 +33,42 @@ class TestResultActivity : ComponentActivity() {
         setContent {
             CarpickAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Page()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Page() {
+    val scaffoldState = rememberBackdropScaffoldState(initialValue = BackdropValue.Concealed)
+    BackdropScaffold(
+        scaffoldState = scaffoldState,
+        modifier = Modifier.fillMaxSize(),
+        appBar = {
+
+        },
+        backLayerContent = {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Color.White
+            ) {
+
+            }
+        },
+        frontLayerContent = {
+            LazyColumn {
+                items(50) {
+                    ListItem(
+                        text = { Text("Item $it", color = Color.White) },
+                    )
+                }
+            }
+        },
+        backLayerBackgroundColor = Color.White,
+        frontLayerBackgroundColor = popupBackground,
+        frontLayerShape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp)
     )
 }
 
@@ -41,6 +76,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview2() {
     CarpickAppTheme {
-        Greeting("Android")
+        Page()
     }
 }
