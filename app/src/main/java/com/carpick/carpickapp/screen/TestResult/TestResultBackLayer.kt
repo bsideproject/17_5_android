0a0e4a8b29fa8af1f30751ff548ee483350c83d9
minwoo129 <key0129mw@naver.com>
2023-12-10 16:03:19 +0900

    추천 결과 페이지 상단부 차량 이미지 영역 UI 적용

--- app/src/main/java/com/carpick/carpickapp/screen/TestResult/TestResultBackLayer.kt
+++ app/src/main/java/com/carpick/carpickapp/screen/TestResult/TestResultBackLayer.kt
@@ -1,22 +1,156 @@
 package com.carpick.carpickapp.screen.TestResult
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.carpick.carpickapp.screen.CarListItem
+import com.carpick.carpickapp.screen.ui.theme.popupBackground
 
 @Composable
-fun TestResultBackLayer() {
-    Box(
+fun TestResultBackLayer(
+    testCarList: MutableList<CarListItem>,
+    selectedIdx: Int,
+    onPressCarRankListItem: (idx: Int) -> Unit
+) {
+    val selectedItem = testCarList.filter { it.idx == selectedIdx }[0]
+    Column(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp)
             .background(Color.White)
     ) {
+        Text(
+            text = "나에게\n가장 어울리는 차는",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp, 32.dp, 0.dp, 0.dp),
+            fontSize = 18.sp,
+            color = popupBackground,
+            fontWeight = FontWeight(700)
+        )
+        Text(
+            text = selectedItem.name,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp, 8.dp, 0.dp, 0.dp),
+            fontSize = 18.sp,
+            color = popupBackground,
+            fontWeight = FontWeight(700)
+        )
+        Text(
+            text = selectedItem.simpleInfo,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp, 8.dp, 0.dp, 8.dp),
+            fontSize = 14.sp,
+            color = popupBackground,
+            fontWeight = FontWeight(400)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 0.dp, 0.dp, 32.dp)
+            ,
+            horizontalArrangement = Arrangement.End
+        ) {
+            Image(
+                painter = painterResource(id = selectedItem.carImg),
+                contentDescription = "테스트용 자동차 이미지",
+                modifier = Modifier
+                    .width(360.dp)
+                    .height(170.dp),
+            )
+        }
+        CarRankListView(testCarList, selectedIdx, onPressCarRankListItem)
 
     }
+}
+
+@Composable
+fun CarRankListView(
+    testCarList: MutableList<CarListItem>,
+    selectedIdx: Int,
+    onPressCarRankListItem: (idx: Int) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 16.dp, 0.dp, 24.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        for (i in 0 until testCarList.size) {
+            CarRankListItem(
+                testCarList[i],
+                i == testCarList.size-1,
+                onPressCarRankListItem,
+                selectedIdx
+            )
+        }
+    }
+}
+
+@Composable
+fun CarRankListItem(
+    item: CarListItem,
+    isLastIdx: Boolean,
+    onPressCarRankListItem: (idx: Int) -> Unit,
+    selectedIdx: Int,
+) {
+    val width = if(isLastIdx) 50 else 60
+    val color = if(selectedIdx == item.idx) popupBackground else Color(0xFFD4D4E1)
+
+    Row(
+        modifier = Modifier.width(width = width.dp),
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Column(
+            modifier = Modifier
+                .width(50.dp)
+                .clickable {
+                    onPressCarRankListItem(item.idx)
+                },
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .width(50.dp)
+                    .height(50.dp)
+                    .background(color, shape = RoundedCornerShape(9.dp)),
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = item.carImg),
+                    contentDescription = "차 아이템",
+                    modifier = Modifier.size(50.dp)
+                )
+            }
+
+            Text(
+                text = "${item.rank}순위",
+                fontSize = 12.sp,
+                fontWeight = FontWeight(600),
+                color = color
+            )
+        }
+    }
+
 }
--- app/src/main/java/com/carpick/carpickapp/screen/TestResultActivity.kt
+++ app/src/main/java/com/carpick/carpickapp/screen/TestResultActivity.kt
@@ -4,51 +4,25 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.BackdropScaffold
-import androidx.compose.material.BackdropValue
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ListItem
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.rememberBackdropScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.carpick.carpickapp.R
 import com.carpick.carpickapp.screen.TestResult.TestResultBackLayer
 import com.carpick.carpickapp.screen.TestResult.TestResultDetail
 import com.carpick.carpickapp.screen.TestResult.TestResultFooter
 import com.carpick.carpickapp.screen.TestResult.TestResultHeader
 import com.carpick.carpickapp.screen.ui.theme.CarpickAppTheme
-import com.carpick.carpickapp.screen.ui.theme.popupBackground
 
 class TestResultActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -91,6 +65,15 @@ fun Page(
     onPressAddWishListBtn: () -> Unit
 ) {
     val scrollState = rememberScrollState()
+    val testCarList = mutableListOf<CarListItem>(
+        CarListItem(0, "쏘나타 디 엣지", "2024년형 가솔린 2.0 하이브리드\n프리미엄 A/T", R.drawable.test_car_image, 1),
+        CarListItem(1, "디 올 뉴 코나", "2023년형 가솔린 1.6 하이브리드\n모던 2WD A/T", R.drawable.test_car_image1, 2),
+        CarListItem(2, "디 올 뉴 코나 일렉트릭", "2023년형 전기 (롱레인지)\n프리미엄 A/T", R.drawable.test_car_image1, 3),
+    )
+    var selectedIdx by remember {
+        mutableStateOf(0)
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxSize(),
@@ -105,7 +88,14 @@ fun Page(
                 onPressBack,
                 onPressWishList
             )
-            TestResultBackLayer()
+            TestResultBackLayer(
+                testCarList,
+                selectedIdx,
+                onPressCarRankListItem = {idx ->
+                    selectedIdx = idx
+
+                }
+            )
             TestResultDetail(onPressMoreAtSimpleSpec, onPressRetest)
             TestResultFooter(onPressShareBtn, onPressAddWishListBtn)
         }
@@ -140,3 +130,11 @@ fun GreetingPreview2() {
         )
     }
 }
+
+data class CarListItem(
+    val idx: Int,
+    val name: String,
+    val simpleInfo: String,
+    val carImg: Int,
+    val rank: Int,
+)
