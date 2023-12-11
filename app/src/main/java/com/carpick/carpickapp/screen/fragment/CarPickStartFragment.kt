3c26bcfd9a027b4a37c7666547aed6e9b49b7bf8
idstudent <36175704+idstudent@users.noreply.github.com>
2023-12-11 13:08:15 +0900

    차량 결과 없는 페이지 작업

--- app/src/main/java/com/carpick/carpickapp/screen/fragment/CarPickStartFragment.kt
+++ app/src/main/java/com/carpick/carpickapp/screen/fragment/CarPickStartFragment.kt
@@ -1,12 +1,9 @@
 package com.carpick.carpickapp.screen.fragment
 
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.MediaController
-import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.carpick.carpickapp.R
 import com.carpick.carpickapp.databinding.FragmentCarpickStartBinding
