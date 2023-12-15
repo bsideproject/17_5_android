28de842ad7cdd65fdbcb2f800ae45af1eccb310e
minwoo129 <key0129mw@naver.com>
2023-12-15 21:27:13 +0900

    추천 결과 페이지 networktestviewmodel 테스트 연결

--- app/src/main/java/com/carpick/carpickapp/screen/TestResultActivity.kt
+++ app/src/main/java/com/carpick/carpickapp/screen/TestResultActivity.kt
@@ -13,17 +13,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.carpick.carpickapp.R
 import com.carpick.carpickapp.screen.TestResult.TestResultBackLayer
 import com.carpick.carpickapp.screen.TestResult.TestResultDetail
 import com.carpick.carpickapp.screen.TestResult.TestResultFooter
 import com.carpick.carpickapp.screen.TestResult.TestResultHeader
 import com.carpick.carpickapp.screen.ui.theme.CarpickAppTheme
+import com.carpick.carpickapp.viewModel.NetworkTestViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class TestResultActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +39,7 @@ class TestResultActivity : ComponentActivity() {
             CarpickAppTheme {
                 // A surface container using the 'background' color from the theme
                 Page(
+                    networkTestViewModel = hiltViewModel(),
                     onPressBack = {
                         Log.d("TestResult", "onPressBack")
                     },
@@ -57,6 +66,7 @@ class TestResultActivity : ComponentActivity() {
 
 @Composable
 fun Page(
+    networkTestViewModel: NetworkTestViewModel,
     onPressBack: () -> Unit,
     onPressWishList: () -> Unit,
     onPressMoreAtSimpleSpec: () -> Unit,
@@ -65,6 +75,7 @@ fun Page(
     onPressAddWishListBtn: () -> Unit
 ) {
     val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
     val testCarList = mutableListOf<CarListItem>(
         CarListItem(0, "쏘나타 디 엣지", "2024년형 가솔린 2.0 하이브리드\n프리미엄 A/T", R.drawable.test_car_image, 1, 31870000),
         CarListItem(1, "디 올 뉴 코나", "2023년형 가솔린 1.6 하이브리드\n모던 2WD A/T", R.drawable.test_car_image1, 2, 29990000),
@@ -72,6 +83,15 @@ fun Page(
     )
     var selectedIdx by remember {
         mutableStateOf(0)
+    }
+
+    fun _onPressWishList() {
+        coroutineScope.launch {
+            networkTestViewModel.getSearchResult().collect {
+                Log.d("TestResultActivity", "result: ${it.toString()}")
+            }
+
+        }
     }
 
     Surface(
@@ -86,7 +106,9 @@ fun Page(
         ) {
             TestResultHeader(
                 onPressBack,
-                onPressWishList
+                onPressWishList = {
+                    _onPressWishList()
+                }
             )
             TestResultBackLayer(
                 testCarList,
@@ -109,6 +131,7 @@ fun Page(
 fun GreetingPreview2() {
     CarpickAppTheme {
         Page(
+            networkTestViewModel = hiltViewModel(),
             onPressBack = {
                 Log.d("TestResult", "onPressBack")
             },
