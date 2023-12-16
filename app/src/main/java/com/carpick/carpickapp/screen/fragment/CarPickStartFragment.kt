4e961658e1d82c52048f9b33f3b69a780b1244a2
idstudent <sm1374@naver.com>
2023-12-16 21:50:29 +0900

    백키클릭시 스택정리

--- app/src/main/java/com/carpick/carpickapp/screen/fragment/CarPickStartFragment.kt
+++ app/src/main/java/com/carpick/carpickapp/screen/fragment/CarPickStartFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.FragmentManager
 import com.bumptech.glide.Glide
 import com.carpick.carpickapp.R
 import com.carpick.carpickapp.databinding.FragmentCarpickStartBinding
@@ -26,6 +28,12 @@ class CarPickStartFragment : BaseFragment<FragmentCarpickStartBinding>() {
                 transaction.replace(R.id.nav_host, newFragment)
                 transaction.addToBackStack(null)
                 transaction.commit()
+            }
+
+            requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+                val fragmentManager = requireActivity().supportFragmentManager
+                fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                activity?.finish()
             }
         }
     }
