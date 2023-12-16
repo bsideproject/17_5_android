324afc77b586bf4c27ab7091910f1b61cd87d504
minwoo129 <key0129mw@naver.com>
2023-12-16 13:45:42 +0900

    차량 추천 페이지 내 상세 스펙 페이지 헤더 추가

---
+++ app/src/main/java/com/carpick/carpickapp/screen/DetailSpec/DetailSpecHeader.kt
@@ -0,0 +1,63 @@
+package com.carpick.carpickapp.screen.DetailSpec
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.carpick.carpickapp.R
+
+@Composable
+fun DetailSpecHeader(
+    onPressBack: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(54.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp, 12.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_back_white),
+                contentDescription = "뒤로가기",
+                modifier = Modifier
+                    .size(30.dp)
+                    .clickable {
+                        onPressBack()
+                    },
+            )
+
+            Text(
+                text = "상세스펙",
+                fontSize = 16.sp,
+                color = Color.White,
+                fontWeight = FontWeight(600)
+            )
+
+            Box(
+                modifier = Modifier.size(30.dp)
+            )
+        }
+    }
+}
--- app/src/main/java/com/carpick/carpickapp/screen/DetailSpecActivity.kt
+++ app/src/main/java/com/carpick/carpickapp/screen/DetailSpecActivity.kt
@@ -1,16 +1,22 @@
 package com.carpick.carpickapp.screen
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.carpick.carpickapp.screen.DetailSpec.DetailSpecHeader
 import com.carpick.carpickapp.screen.ui.theme.CarpickAppTheme
+import com.carpick.carpickapp.screen.ui.theme.popupBackground
 
 class DetailSpecActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,27 +24,33 @@ class DetailSpecActivity : ComponentActivity() {
         setContent {
             CarpickAppTheme {
                 // A surface container using the 'background' color from the theme
-                DetailSpecPage()
+                DetailSpecPage(
+                    onPressBack = {
+                        finish()
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun DetailSpecPage() {
+fun DetailSpecPage(
+    onPressBack: () -> Unit
+) {
+    val scrollState = rememberScrollState()
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
+        color = popupBackground
     ) {
-        Greeting("Android")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            DetailSpecHeader(
+                onPressBack
+            )
+        }
     }
 }
 
@@ -46,6 +58,10 @@ fun DetailSpecPage() {
 @Composable
 fun GreetingPreview4() {
     CarpickAppTheme {
-        DetailSpecPage()
+        DetailSpecPage(
+            onPressBack = {
+                Log.d("DetailSpecActivity", "onPressBack")
+            }
+        )
     }
 }
